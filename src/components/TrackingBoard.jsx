b96baad33ba722519218a5dbb8ef1cb626034ca0
Md Tanzil <mdtanzil.py@gmail.com>
2024-10-17 01:13:27 +0600

    template convert to react

--- src/components/TrackingBoard.jsx
+++ src/components/TrackingBoard.jsx
@@ -1,5 +1,21 @@
+import BalanceStat from "./BalanceStat";
+import Expense from "./Expense";
+import Income from "./Income";
+import SubmissionForm from "./SubmissionForm";
+
 const TrackingBoard = () => {
-  return <div></div>;
+  return (
+    <>
+      <SubmissionForm />
+      <div className="lg:col-span-2">
+        <BalanceStat />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
+          <Expense />
+          <Income />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TrackingBoard;
